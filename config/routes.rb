9a409b0ddb266780135e62ce89378077970907b2
Ammar <marmar18@live.com>
2021-09-22 13:32:20 -0500

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
   get 'service_logger/service'
   get 'point_tracker/tracker'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
