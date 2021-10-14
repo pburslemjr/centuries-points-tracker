d3d02f1f426648d03b5182194217e5238c1b3205
pburslemjr <pburslemjr@gmail.com>
2021-10-13 22:14:49 -0500

    Added Login functionality

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -65,7 +65,7 @@ class LoginController < ApplicationController
           logger.error "Google authentication error: #{error}"
           nil
         else
-          redirect_to(service_logger_index_path)
+          redirect_to(events_path)
         
         end
       end
