5665a57e56e6e2a1a57e3ba22e05a07b135367bd
pburslemjr <pburslemjr@gmail.com>
2021-10-13 22:05:29 -0500

    Added Login functionality

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -16,23 +16,25 @@ class LoginController < ApplicationController
     
   
     def click
-      if :doCreate
+      if :doCreate == false
         if user = authenticate_with_google
           cookies.signed[:user_id] = user.id
+          cookies[:debug] = "doCreate = f  auth = t"
           
                 
           redirect_to(home_index_url)
         else
-          
+          cookies[:debug] = "doCreate = f  auth = f"
           cookies.signed[:user_id] = 'created account I think'
           redirect_to(home_index_url)
         end
       else
         if user = create
+          cookies[:debug] = "doCreate = t  auth = t"
           cookies.signed[:user_id] = user.id
           redirect_to(home_index_url)
         else
-          
+          cookies[:debug] = "doCreate = t  auth = f"
           cookies.signed[:user_id] = 'failed to create account'
           redirect_to(home_index_url)
         end
@@ -57,7 +59,7 @@ class LoginController < ApplicationController
           cookies.signed[:user_id] = GoogleSignIn::Identity.new(id_token).user_id
           Member.create(name: GoogleSignIn::Identity.new(id_token).name, email_id: cookies.signed[:user_id])
           Member.find_by email_id: cookies.signed[:user_id]
-          
+
           
         elsif flash[:google_sign_in] && error = flash[:google_sign_in][:error]
           logger.error "Google authentication error: #{error}"
