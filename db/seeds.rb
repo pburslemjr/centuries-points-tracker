3bc555f9613e2faf8f3accbd4187b379032142df
pburslemjr <pburslemjr@gmail.com>
2021-10-22 16:33:28 -0500

    populated seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+whitelist_data = [
+  [ "paul-b-tamu@tamu.edu", false ],
+  [ "centurymens.social@gmail.com", true ],
+  [ "ammar918@gmail.com", true ],
+  [ "siddiqi918@tamu.edu", false ],
+  [ "deananderson@tamu.edu", false ],
+  [ "andersondeant@gmail.com", true ],
+  [ "mivoli98@tamu.edu", false ],
+  [ "mibeophi2@gmail.com", true ]
+  
+]
+
+whitelist_data.each do |email, isAdmin|
+  Whitelist.create( email: email, isAdmin: isAdmin )
+end
+
