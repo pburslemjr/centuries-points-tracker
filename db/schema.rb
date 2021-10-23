fc6d6ab6b272716b8b66d3fb8c2c6a6e7e675447
Ammar <marmar18@live.com>
2021-10-23 12:08:12 -0500

    Dropped 'attendence_list' table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_031706) do
+ActiveRecord::Schema.define(version: 2021_10_23_170646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,11 +20,6 @@ ActiveRecord::Schema.define(version: 2021_10_23_031706) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "member_id"
-  end
-
-  create_table "attendence_list", force: :cascade do |t|
-    t.integer "member_id"
-    t.integer "event_id"
   end
 
   create_table "events", force: :cascade do |t|
