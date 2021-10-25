b70cdc06773252bfb91c4559f993452859e75c94
Ammar <marmar18@live.com>
2021-10-25 12:55:41 -0500

    fixed rubocop correction inverting bool logic

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,7 +3,11 @@ class Member < ApplicationRecord
   has_and_belongs_to_many :events
 
   def self.from_google(uid:, full_name:, email:)
-    return nil unless Whitelist.find_by(email: email).nil?
+    return nil unless not Whitelist.find_by(email: email).nil?
+
+      unless Whitelist.find_by(email: email) != nil
+        return nil
+      end
 
     create_with(uid: uid, name: full_name, email: email,
                 isAdmin: Whitelist.find_by(email: email).isAdmin).find_or_create_by!(uid: uid)
