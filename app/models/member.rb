8a0e96136ecaded84be8c2410be41abe9d3ce0d2
DangerousDean <44074418+DangerousDean@users.noreply.github.com>
2021-10-24 19:20:21 -0500

    fixed log in page notices and spacing

--- app/models/member.rb
+++ app/models/member.rb
@@ -3,7 +3,9 @@ class Member < ApplicationRecord
     has_and_belongs_to_many :events
 
     def self.from_google(uid:, full_name:, email:)
-      print(email , "##############################################")
+      unless Whitelist.find_by(email: email) != nil
+        return nil
+      end
       create_with(uid: uid, name: full_name, email: email, isAdmin: Whitelist.find_by(email: email).isAdmin).find_or_create_by!(uid: uid)
     end
   
