0a06bbfa50d89f14671d4880285ba67ff43667f4
pburslemjr <pburslemjr@gmail.com>
2021-11-02 14:41:18 -0500

    Fixed Events page bug. Added ordering to member list

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -1,11 +1,68 @@
 class MemberController < ApplicationController
   def index
-    @members = Member.all.order('name desc')
+    if params[:reverse] == "true"
+      @reverse = true
+    elsif params[:reverse] == false
+      @reverse = false
+    else
+      @reverse = false
+    end
+    
+    if params[:sort].nil?
+      resetSorting
+      
+    end
+    if params[:sort] == "pp"
+      if !@reverse
+        
+        @members = Member.all.sort_by{|member| member.getPP}.reverse!
+      else 
+        @members = Member.all.sort_by{|member| member.getPP}
+      end
+    elsif params[:sort] == "mm"
+      if !@reverse
+        @members = Member.all.sort_by{|member| member.getMM}.reverse!
+      else 
+        @members = Member.all.sort_by{|member| member.getMM}
+      end
+    elsif params[:sort] == "hours"
+      if !@reverse
+        @members = Member.all.sort_by{|member| member.getService}.reverse!
+      else 
+        @members = Member.all.sort_by{|member| member.getService}
+      end
+    else
+      if @reverse
+        @members = Member.all.order(params[:sort]).reverse
+      else 
+        @members = Member.all.order(params[:sort])
+      end
+      
+    end
     ordered = Event.order(:datetime)
    
     @past_events = ordered.where('datetime <= ?', Time.now)
     
   end
+
+  def resetSorting
+    @name_order = "asc"
+    @email_order = ""
+    @pp_order = ""
+    @mm_order = ""
+    @service_order = ""
+  end
+
+  def getSortArrow    
+    if params[:reverse] == "true"      
+      return '#8681;'
+    else params[:reverse] == "false"      
+      return '#8679;'
+    
+    end
+  end
+
+  helper_method :getSortArrow
   
   def show
     @member = Member.find_by(id: params[:id])
