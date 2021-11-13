47e53bc18b1ef942dc1084d45541ee4171ecbec1
DangerousDean <44074418+DangerousDean@users.noreply.github.com>
2021-11-12 18:15:15 -0600

    Revert "updated main participation page for admin and member"

--- db/migrate/20211111014240_remove_admin_from_whitelist.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAdminFromWhitelist < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :whitelists, :isAdmin
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_014240) do
+ActiveRecord::Schema.define(version: 2021_10_23_190032) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_11_11_014240) do
 
   create_table "whitelists", force: :cascade do |t|
     t.string "email", null: false
+    t.boolean "isAdmin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
