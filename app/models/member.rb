90c84ddbd0b90d2b6bea9e83cb62cfdc9e82190c
Paul Burslem <pburslemjr@gmail.com>
2022-03-02 17:39:44 -0600

    Tried to fix issue

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,7 +5,7 @@ class Member < ApplicationRecord
   def self.from_google(uid:, full_name:, email:)
      #print("UID '#{uid}', full_name '#{full_name}', email '#{email}'\n")
 
-    return nil if Whitelist.where("lower(email) = ?", email.downcase).nil?
+    return nil if Whitelist.where("lower(email) = ?", email.downcase).blank?
 
 
     create_with(uid: uid, name: full_name, email: email.downcase, isAdmin: false).find_or_create_by!(uid: uid)
