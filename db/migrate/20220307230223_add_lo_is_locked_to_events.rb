92bc230b8889803e70a8b949dc5a8f00b4f6def8
paul-b-tamu <paul-b-tamu@tamu.edu>
2022-03-07 19:18:14 -0600

    Fixed migrate bug

--- db/migrate/20220307230223_add_lo_is_locked_to_events.rb
+++
@@ -1,6 +0,0 @@
-class AddLoIsLockedToEvents < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :events, :isLocked, :string
-    add_column :events, :isLocked, :boolean
-  end
-end
--- db/migrate/20220307230810_change_default_is_locked.rb
+++ db/migrate/20220307230810_change_default_is_locked.rb
@@ -1,6 +1,5 @@
 class ChangeDefaultIsLocked < ActiveRecord::Migration[6.1]
   def change
-    remove_column :events, :isLocked, :boolean
     add_column :events, :isLocked, :boolean, default: false
   end
 end
